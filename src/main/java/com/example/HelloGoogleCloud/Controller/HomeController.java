7ce902eca55cfbd824ff15e228d455db351aa8b2
HP <kumarmanoj11083@gmail.com>
2021-01-17 21:10:25 +0530

    Commit

--- src/main/java/com/example/HelloGoogleCloud/Controller/HomeController.java
+++ src/main/java/com/example/HelloGoogleCloud/Controller/HomeController.java
@@ -1,21 +1,21 @@
 package com.example.HelloGoogleCloud.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.HelloGoogleCloud.Service.HomeService;
 
-@Controller
+@RestController
 public class HomeController {
 
 	@Autowired
 	HomeService service;
 
-	@RequestMapping(value="/login", method = RequestMethod.GET)
+	@RequestMapping(value="/", method = RequestMethod.GET)
 	public String showLoginPage(ModelMap model) {
 		return "login";
 	}
